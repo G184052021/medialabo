c2fe76aef1e5996e43b41fe5d1c5c133b301b419
G184052021 <218405@st.takushoku-u.ac.jp>
2022-05-19 12:23:52 +0900

    第四回

---
+++ practice-dom/dom-demo.js
@@ -0,0 +1,8 @@
+let ul=document.querySelector('ul');
+let li;
+li=document.createElement('li');
+li.textContent='アジア大陸';
+ul.insertAdjacentElement('beforeend', li);
+li = document.createElement('li');
+li.textContent = 'ヨーロッパ大陸'; 
+ul.insertAdjacentElement('beforeend', li);
