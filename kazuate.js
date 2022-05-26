4744bb27e3f02ac7a70cd745aa3dcfc24279eb37
G184052021 <218405@st.takushoku-u.ac.jp>
2022-05-26 14:55:09 +0900

    第五回

--- kazuate.js
+++ kazuate.js
@@ -7,7 +7,7 @@ let kaisu = 0;
 
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-
+hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
@@ -17,7 +17,7 @@ function hantei() {
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
-    
+    s.textContent=kaisu+'回目の予想:'+yoso;
      if(kotae==yoso) {
          s.textContent='正解です．おめでとう!';
         } else if(kotae>yoso) {
