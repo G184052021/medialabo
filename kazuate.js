175ae6db7f8d399a4891c911ad9442f35b8e5835
G184052021 <218405@st.takushoku-u.ac.jp>
2022-05-26 14:49:44 +0900

    第五回

--- kazuate.js
+++ kazuate.js
@@ -3,33 +3,28 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え(デバッグ用): ' + kotae);      // デバッグ用
 
 // 入力回数（予想回数）
-let kaisu = 1;
+let kaisu = 0;
 
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
-hantei();
 
-let hantei=hantei();
-let b = document.querySelector('#answer');
-b.addEventListener('click', hantei);
+
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    
+    let s=document.querySelector('p#result');
     let i = document.querySelector('input[name="yoso"]');
     let yoso = i.value;       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
-    console.log(kaisu+'回目の予想:'+yoso);
     
      if(kotae==yoso) {
-         console.log('正解です．おめでとう!');
+         s.textContent='正解です．おめでとう!';
         } else if(kotae>yoso) {
-            console.log('まちがい．答えはもっと大きいですよ');
+            s.textContent='まちがい．答えはもっと大きいですよ';
         } else {
-            console.log('まちがい．答えはもっと小さいですよ');
+            s.textContent='まちがい．答えはもっと小さいですよ';
         }       
     
 } 
-let s=document.querySelector('div#result');
-s.textContent=hantei;
+
