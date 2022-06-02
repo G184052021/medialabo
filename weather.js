aa72da305f17be005e579fb1e8be79972cef42b4
G184052021 <218405@st.takushoku-u.ac.jp>
2022-06-02 15:26:46 +0900

    第六回

--- weather.js
+++ weather.js
@@ -1,51 +1,49 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
+let b=document.querySelector('#kensaku');
+b.addEventListener('click', kensaku);
 
-////////// 課題3-2 ここからプログラムを書こう
-console.log('都市名:'+data.name);
-console.log('最高気温:'+data.main.temp_max);
-console.log('最低気温:'+data.main.temp_min);
+function kensaku() {
+  let i = document.querySelector('input[name="toshi"]');
+  let key = i.value;
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+key+'.json';
+  axios.get(url)
+       .then(showResult)
+       .catch(showError)
+       .then(finish);
+}
+function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+  let s1=document.querySelector('p#result1');
+  let s2=document.querySelector('p#result2');
+  let s3=document.querySelector('p#result3');
+  let s4=document.querySelector('p#result4');
+  let s5=document.querySelector('p#result5');
+  let s6=document.querySelector('p#result6');
+  s1.textContent='都市名:'+data.name;
+  s2.textContent='緯度:'+data.coord.lon+'　　経度:'+data.coord.lat;
+  for(const value of data.weather) {
+    s3.textContent='天気:'+value.description;
+  }
+  s4.textContent='最低気温:'+data.main.temp_min+'　　最高気温:'+data.main.temp_max;
+  s5.textContent='湿度:'+data.main.humidity;
+  s6.textContent='風速:'+data.wind.speed+'　　風向:'+data.wind.deg;
+
+  console.log(data);
+  console.log('都市名:'+data.name);
+  console.log('緯度:'+data.coord.lon+'経度:'+data.coord.lat);
+  for(const value of data.weather) {
+    console.log('天気:'+value.description)
+  }
+  
+  console.log('最低気温:'+data.main.temp_min+'最高気温:'+data.main.temp_max);
+  console.log('湿度:'+data.main.humidity);
+  console.log('風速:'+data.wind.speed+'風向:'+data.wind.deg);
+}
+function showError(err) {
+  console.log(err);
+}
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
